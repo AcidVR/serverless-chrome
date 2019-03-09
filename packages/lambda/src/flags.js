42025476a8fcecb39d617b9049b09dfb40987a62
Bill Landers <landers.bill@outlook.com>
2019-03-09 00:03:16 -0500

    gpu

--- packages/lambda/src/flags.js
+++ packages/lambda/src/flags.js
@@ -6,10 +6,6 @@ export default [
   ...LOGGING_FLAGS,
   '--disable-dev-shm-usage', // disable /dev/shm tmpfs usage on Lambda
 
-  // @TODO: review if these are still relevant:
-  '--disable-gpu',
-  '--single-process', // Currently wont work without this :-(
-
   // https://groups.google.com/a/chromium.org/d/msg/headless-dev/qqbZVZ2IwEw/Y95wJUh2AAAJ
   '--no-zygote', // helps avoid zombies
 
